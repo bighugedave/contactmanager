0b363d836c61bd07215ac089cbee4ba1aca19841
David Ayres <superdavidayres@fastmail.com>
2019-11-09 17:27:18 -0500

    started adding form for adding user

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
+import AddContact from "./components/contacts/AddContact";
 import { Provider } from "./context";
 import "materialize-css/dist/css/materialize.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faPlusSquare, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faPlusSquare, faTimes, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
-library.add(fab, faPlusSquare, faTimes);
+library.add(fab, faPlusSquare, faTimes, faPlusCircle);
 
 function App() {
     return (
         <Provider>
             <div>
                 <Header branding="Contact Manager" />
+                <AddContact />
                 <Contacts />
             </div>
         </Provider>
