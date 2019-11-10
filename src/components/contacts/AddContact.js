8fc22994a58cf1cdfffbcc4883d8289fc3d45ac9
David Ayres <superdavidayres@fastmail.com>
2019-11-10 12:33:46 -0500

    added form submit functionality

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class AddContact extends Component {
 
@@ -9,9 +8,12 @@ class AddContact extends Component {
         phone: ''
     }
 
-    onNameChange = (e) => this.setState({name: e.target.value});
-    onEmailChange = (e) => this.setState({email: e.target.value});
-    onPhoneChange = (e) => this.setState({phone: e.target.value});
+    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
 
     render() {
 
@@ -23,34 +25,27 @@ class AddContact extends Component {
                     <div className="card blue darken-2">
                         <div className="card-content">
                             <span className="card-title white-text">Add Contact</span>
-                            <p className="white-text">Add Contact</p>
-                            <form className="container">
+                            <form className="container" onSubmit={this.onFormSubmit} style={{ margin: "0px" }}>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="name" type="text" placeholder="Name" className="validate white-text" 
-                                        value={name} onChange={this.onNameChange} />
+                                        <input id="name" name="name" type="text" placeholder="Name" className="validate white-text" 
+                                        value={name} onChange={this.onChange} />
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="email" type="email" placeholder="Email" className="validate white-text" 
-                                        value={email} onChange={this.onEmailChange} />
+                                        <input id="email" name="email" type="email" placeholder="Email" className="validate white-text" 
+                                        value={email} onChange={this.onChange} />
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="phone" type="text" placeholder="Phone" className="validate white-text" 
-                                        value={phone} onChange={this.onPhoneChange} />
+                                        <input id="phone" name="phone" type="text" placeholder="Phone" className="validate white-text" 
+                                        value={phone} onChange={this.onChange} />
                                     </div>
                                 </div>
-                                <FontAwesomeIcon
-                                    icon="plus-circle"
-                                    style={{
-                                        cursor: "pointer",
-                                        fontSize: "25px",
-                                        color: "white"
-                                    }}
-                                />
+                                <button className="btn waves-effect blue darken-4" type="submit" name="action">Submit</button>
+                                
                             </form>
                         </div>
                     </div>
